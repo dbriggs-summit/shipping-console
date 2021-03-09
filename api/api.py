697ada97cab3ecdd89baa066671cc6a5311e93c8
davidb <davidb@summitappliance.com>
2021-03-09 14:26:19 -0500

    Initial commit - DOES NOT RUN

--- api/api.py
+++ api/api.py
@@ -1,30 +1,41 @@
 import time
-from sqlalchemy import create_engine
-import sqlite3
+from sqlalchemy import create_engine, exc
+from sqlalchemy.orm import sessionmaker
 
 from flask import Flask
 import config
 
-server = config.server
-database = config.databaseName
-userName = config.userName
-password = config.password
-engine = create_engine(f'mssql+pyodbc://{userName}:{password}@{server}/{database}?driver=SQL Server')
+dyna_server = config.dynaServer
+dyna_db = config.dynaDBName
+dyna_user = config.dynaUserName
+dyna_password = config.dynaPassword
+postgres_server = config.postgresServer
+postgres_db = config.postgresDBName
+postgres_user = config.postgresUserName
+postgres_password = config.postgresPassword
 
+dynacom_eng = \
+    create_engine(f'mssql+pyodbc://{dyna_user}:{dyna_password}@{dyna_server}/'
+                  f'{dyna_db}?driver=SQL Server')
+postgres_eng = \
+    create_engine(f'postgresql://{postgres_user}:{postgres_password}@'
+                  f'{postgres_server}:5432/{postgres_db}')
 
 app = Flask(__name__)
 
-#set up persistent data store for orders that lives outside of query lifetime
+
+# set up persistent data store for orders that lives outside of query lifetime
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
 
+
 @app.cli.command()
 def order_poll():
     date = '\'2021-03-04\''  # 'getdate()'
 
-    with engine.connect() as con:
+    with dynacom_eng.connect() as con:
         rs = con.execute(
             f"""select h.invoiid as id, 
                 h.shipvia as ship_via, 
@@ -41,44 +52,57 @@ def order_poll():
                 isnull(i.UPCCode,'') as upc_code,
                 i.ItemId as item_id,
                 d.InInvoiDetId as line_id
-            from InvoiHdr h inner join InvoiDet d on h.InInvoiId = d.ExInvoiId
-            inner join Item i on d.ExItemId = i.InItemId
-            inner join Warehouse w on d.usrShipFromWarehouse = w.InWarehouseId
+            from InvoiHdr as h inner join InvoiDet as d on h.InInvoiId = d.ExInvoiId
+            inner join Item as i on d.ExItemId = i.InItemId
+            inner join Warehouse as w on d.usrShipFromWarehouse = w.InWarehouseId
             where h.[x04472474_ShippedDate] = {date} AND h.[InvoiType] = 51 AND 
-                d.[usrShipFromWarehouse] = 4 AND h.[x04472474_Shipped] = 1 AND h.[ShipVia] != '' 
-                and i.itemid not in ('j4g','j5u','x1','j4')
+                d.[usrShipFromWarehouse] = 4 AND h.[x04472474_Shipped] = 1 AND 
+                h.[ShipVia] != '' and i.itemid not in ('j4g','j5u','x1','j4')
         """)
 
     new_orders = []
     idx = 0
     order_list = {}  # store pos so we can insert if lines aren't in order
     for line in rs:
-        #look for unique header record
+        # look for unique header record
         if line.id not in order_list:
-            new_orders.append({ #insert header record
-                "id":line['id'],
-                "ship_via":line['ship_via'],
-                "order_id":line['order_id'],
-                "ship_date":line['ship_date'],
-                "order_type":line['order_type'],
-                "status":line['status'],
-                "lines":[]
+            new_orders.append({  # insert header record
+                "id": line['id'],
+                "ship_via": line['ship_via'],
+                "order_id": line['order_id'],
+                "ship_date": line['ship_date'],
+                "order_type": line['order_type'],
+                "status": line['status'],
+                "lines": []
             })
             order_list[line['id']] = idx
             idx += 1
+
         new_orders[order_list[line['id']]]["lines"].append({
-            "qty" : line['qty'],
-            "qty_scanned" : line['qty_scanned'],
-            "ship_from" : line['ship_from'],
-            "upc_code" : line['upc_code'],
-            "item_id" : line['item_id'],
-            "line_id" : line['line_id']
+            "qty": line['qty'],
+            "qty_scanned": line['qty_scanned'],
+            "ship_from": line['ship_from'],
+            "upc_code": line['upc_code'],
+            "item_id": line['item_id'],
+            "line_id": line['line_id']
         })
+    # update data store with new_orders
+    Session = sessionmaker(bind=postgres_eng)
+    session = Session()
 
-    #update data store with new_orders
+    try:
+        session.execute('delete from orders')
+        for order in new_orders:
+            session.execute(f'insert into orders values({order["id"]}, {order})')
+        session.commit()
+    except exc.SQLAlchemyError as e:
+        print(e)
+        session.rollback()
+    finally:
+        session.close()
 
-#@app.route('/update_line')
-#def update_line():
+# @app.route('/update_line')
+# def update_line():
 
-#@app.route('/update_shipment')
-#def update_shipment():
+# @app.route('/update_shipment')
+# def update_shipment():
